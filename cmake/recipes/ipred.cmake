c0b698bfec5d9e87794915048a8f218e34399ebb
Suraj Musuvathy <surajmusuvathy@ntopology.com>
2022-01-11 17:24:03 -0500

    use ntop fork of indirect predicates repo

--- cmake/recipes/ipred.cmake
+++ cmake/recipes/ipred.cmake
@@ -7,8 +7,8 @@ message(STATUS "Third-party: creating target 'ipred'")
 include(FetchContent)
 FetchContent_Declare(
     ipred
-    GIT_REPOSITORY https://github.com/teseoch/Indirect_Predicates.git
-    GIT_TAG a0bf2cd73383202b491a3d8d89f65a8efff232b1
+    GIT_REPOSITORY https://github.com/nTopology/Indirect_Predicates.git
+    GIT_TAG 3ae5d32bc235994c23ed3d33e9b22eeb2ab85b35
     GIT_SHALLOW TRUE
 )
 FetchContent_MakeAvailable(ipred)
